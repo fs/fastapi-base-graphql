0ef17685336005b8341f8a32d833143fedb1f494
RamilAbzalov <ramil.abzalov@scalewill.com>
2022-04-04 16:52:09 +0300

    -

---
+++ app/core/security.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Check password hash."""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """Generate password hash."""
+    return pwd_context.hash(password)
