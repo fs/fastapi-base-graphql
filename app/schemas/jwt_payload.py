4d23a925f97bf5a40012817f8d29bc37fa7c3ef9
Artyom Elin <artyom.elin@scalewill.com>
2022-04-05 14:35:31 +0300

    Adapted to python-base-graphql schema

--- app/schemas/jwt_payload.py
+++ app/schemas/jwt_payload.py
@@ -28,12 +28,6 @@ class RefreshTokenPayload(BaseModel):
     user_id: int
     jti: str
 
-    @validator('scope')
-    def scope_validate(cls, value):
-        if value != 'refresh_token':
-            raise ValueError('Invalid scope for token')
-        return value
-
     @validator('jti')
     def is_revoked(cls, value):
         db_obj = refresh_token.get_by_jti(jti=value)
