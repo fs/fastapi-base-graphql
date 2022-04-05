dca0bd6c739c4d2ad73b5dfd3f8b00fbb15c0621
RamilAbzalov <ramil.abzalov@scalewill.com>
2022-04-05 14:36:04 +0300

    -

---
+++ app/graphql/inputs/__init__.py
@@ -0,0 +1,4 @@
+from app.graphql.inputs.authentication import SignInInput, SignOutInput
+from app.graphql.inputs.users import CreateUserInput, UpdateUserInput
+
+__all__ = ['SignOutInput', 'SignInInput', 'CreateUserInput', 'UpdateUserInput']
--- app/graphql/inputs/users.py
+++ app/graphql/inputs/users.py
@@ -1,8 +1,13 @@
 import strawberry
 
-from app.schemas import UserCreate
+from app.schemas import UserCreate, UserUpdate
 
 
 @strawberry.experimental.pydantic.input(model=UserCreate, all_fields=True)
 class CreateUserInput:
     """Create user input."""
+
+
+@strawberry.experimental.pydantic.input(model=UserUpdate, all_fields=True)
+class UpdateUserInput:
+    """Update user fields mutation input."""
